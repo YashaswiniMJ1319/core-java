804eaeffb44258b720c1968abdb5c5982a7bff91
YashaswiniMJ1319 <mjyr1319@gmail.com>
2026-03-30 00:47:40 +0530

    new commit

---
+++ OOP's instance methods/Ethernet.java
@@ -0,0 +1,21 @@
+class Ethernet{
+	String brandName = "Net";
+    String type = "Wire";
+    int ports = 6;
+    float speed = 10012.0f;
+    char connectivity = 'A';
+	
+     String model; 
+     float cableLength; 
+     float bandwidth;
+     char code; 
+     int price;
+     int users;
+     float size; 
+     char quality; 
+     int discount;
+	
+	public Ethernet(){
+		
+	}
+}
---
+++ OOP's instance methods/Fire.java
@@ -0,0 +1,23 @@
+class Fire{
+	String brandName="Phoenix";
+	String type="Extinguisher";
+	int capacity = 5; 
+    float range = 3.5f; 
+    char classType = 'A';
+	
+	String model;
+    int weight;
+    float pressure;
+    char code;
+    double price;
+    int refillLife;
+    int users;
+    float size;
+    char quality;
+    double discount;
+	
+	public Fire(){
+		
+	}
+}       
+
